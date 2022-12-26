0a663abb1f004de758446279cdde1abcf94ea478
tony <nguyenhuytoan1994@outlook.com>
2022-12-26 07:09:28 +0700

    Add qr-code

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,35 @@
+import QRCODE from "../../images/qr-code.jpeg";
+
 const Footer = () => {
   return (
     <footer className="toanhuyen-site-footer">
       <div className="toanhuyen-lower-footer">
         <div className="container">
+          <div className="row">
+            <div className="col col-xl-6  col-lg-6 col-md-6 col-sm-12 col-12">
+              <div className="widget wpo-service-link-widget">
+                <div className="widget-title">
+                  <h3>QR CODE</h3>
+                </div>
+                <div className="contact-ft">
+                  <ul>
+                    <li>
+                      <i className="fi flaticon-heart"></i>
+                      <div
+                        style={{
+                          borderRadius: 50,
+                          border: "1px solid #ccc",
+                        }}
+                      >
+                        <img src={QRCODE} alt="" />
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+
           <div className="row">
             <div className="col col-xs-12">
               <p className="copyright">
